977c68407be96e9e92709fdfc208c80f09221536
George Stagg <george.stagg@newcastle.ac.uk>
2022-03-10 17:18:33 +0000

    Tweak stringi build script further
    Ensure that AR is set for the build and explicitly remove the large
    data file, rather than excluding in the file packager.

--- src/stringi/build.sh
+++ src/stringi/build.sh
@@ -13,6 +13,7 @@ CXX=em++
 CXX11=em++
 CXX11FLAGS=-std=gnu++11 -D__STRICT_ANSI__ -I/app/build/R-4.1.2/include
 LDFLAGS=-s SIDE_MODULE=1 -s WASM_BIGINT -s ASSERTIONS=1
+AR=emar
 EOF
 
 tar xvf "${PKG_NAME}.tar.gz"
@@ -25,8 +26,9 @@ mkdir -p root/usr/lib/R/library/
 R CMD INSTALL --library="root/usr/lib/R/library" "${PKG_NAME}" --no-docs --no-test-load --no-staged-install --no-byte-compile
 rm -rf "${PKG_NAME}"
 rm "${PKG_NAME}.tar.gz"
+rm "root/usr/lib/R/library/${PKG_NAME}/libs/icudt55l.dat"
 
 mkdir -p "../../dist/${PKG_NAME}/"
-file_packager "../../dist/${PKG_NAME}/${PKG_NAME}.data" --js-output="../../dist/${PKG_NAME}/${PKG_NAME}.js" --preload 'root@/' --use-preload-plugins --exclude '**/icudt55l.dat'
+file_packager "../../dist/${PKG_NAME}/${PKG_NAME}.data" --js-output="../../dist/${PKG_NAME}/${PKG_NAME}.js" --preload 'root@/' --use-preload-plugins
 rm -rf root
 rm ~/.R/Makevars
