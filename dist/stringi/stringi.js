65bfd63e475dcd497dc75925404863bdf08b6fdc
George Stagg <george.stagg@newcastle.ac.uk>
2022-03-11 11:13:36 +0000

    Compile stringi with -Oz, download data separately

--- dist/stringi/stringi.js
+++ dist/stringi/stringi.js
@@ -200,7 +200,169 @@ Module['FS_createPath']("/usr/lib/R/library/stringi", "help", true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/usr/lib/R/library/stringi/NAMESPACE", "start": 0, "end": 6751}, {"filename": "/usr/lib/R/library/stringi/CITATION", "start": 6751, "end": 7372}, {"filename": "/usr/lib/R/library/stringi/LICENSE", "start": 7372, "end": 29374}, {"filename": "/usr/lib/R/library/stringi/AUTHORS", "start": 29374, "end": 31487}, {"filename": "/usr/lib/R/library/stringi/NEWS", "start": 31487, "end": 67350}, {"filename": "/usr/lib/R/library/stringi/DESCRIPTION", "start": 67350, "end": 68775}, {"filename": "/usr/lib/R/library/stringi/INDEX", "start": 68775, "end": 75239}, {"filename": "/usr/lib/R/library/stringi/html/00Index.html", "start": 75239, "end": 116912}, {"filename": "/usr/lib/R/library/stringi/html/R.css", "start": 116912, "end": 118247}, {"filename": "/usr/lib/R/library/stringi/include/stringi.h", "start": 118247, "end": 138026}, {"filename": "/usr/lib/R/library/stringi/include/stringi.cpp", "start": 138026, "end": 184922}, {"filename": "/usr/lib/R/library/stringi/R/stringi", "start": 184922, "end": 185980}, {"filename": "/usr/lib/R/library/stringi/R/stringi.rdb", "start": 185980, "end": 250841}, {"filename": "/usr/lib/R/library/stringi/R/stringi.rdx", "start": 250841, "end": 254074}, {"filename": "/usr/lib/R/library/stringi/Meta/features.rds", "start": 254074, "end": 254206}, {"filename": "/usr/lib/R/library/stringi/Meta/package.rds", "start": 254206, "end": 255452}, {"filename": "/usr/lib/R/library/stringi/Meta/links.rds", "start": 255452, "end": 257869}, {"filename": "/usr/lib/R/library/stringi/Meta/nsInfo.rds", "start": 257869, "end": 259583}, {"filename": "/usr/lib/R/library/stringi/Meta/Rd.rds", "start": 259583, "end": 264298}, {"filename": "/usr/lib/R/library/stringi/Meta/hsearch.rds", "start": 264298, "end": 268815}, {"filename": "/usr/lib/R/library/stringi/libs/stringi.so", "start": 268815, "end": 13015682}, {"filename": "/usr/lib/R/library/stringi/libs/LICENSE.txt", "start": 13015682, "end": 13033915}, {"filename": "/usr/lib/R/library/stringi/help/AnIndex", "start": 13033915, "end": 13043392}, {"filename": "/usr/lib/R/library/stringi/help/aliases.rds", "start": 13043392, "end": 13045328}, {"filename": "/usr/lib/R/library/stringi/help/paths.rds", "start": 13045328, "end": 13046216}, {"filename": "/usr/lib/R/library/stringi/help/stringi.rdb", "start": 13046216, "end": 13494006}, {"filename": "/usr/lib/R/library/stringi/help/stringi.rdx", "start": 13494006, "end": 13496313}], "remote_package_size": 13496313, "package_uuid": "cdef2122-3f7b-40bf-918a-54b82d37a963"});
+   loadPackage({"files": [{"filename": "/usr/lib/R/library/stringi/NAMESPACE", "start": 0, "end": 6751}, {"filename": "/usr/lib/R/library/stringi/CITATION", "start": 6751, "end": 7372}, {"filename": "/usr/lib/R/library/stringi/LICENSE", "start": 7372, "end": 29374}, {"filename": "/usr/lib/R/library/stringi/AUTHORS", "start": 29374, "end": 31487}, {"filename": "/usr/lib/R/library/stringi/NEWS", "start": 31487, "end": 67350}, {"filename": "/usr/lib/R/library/stringi/DESCRIPTION", "start": 67350, "end": 68775}, {"filename": "/usr/lib/R/library/stringi/INDEX", "start": 68775, "end": 75239}, {"filename": "/usr/lib/R/library/stringi/html/00Index.html", "start": 75239, "end": 116912}, {"filename": "/usr/lib/R/library/stringi/html/R.css", "start": 116912, "end": 118247}, {"filename": "/usr/lib/R/library/stringi/include/stringi.h", "start": 118247, "end": 138026}, {"filename": "/usr/lib/R/library/stringi/include/stringi.cpp", "start": 138026, "end": 184922}, {"filename": "/usr/lib/R/library/stringi/R/stringi", "start": 184922, "end": 185980}, {"filename": "/usr/lib/R/library/stringi/R/stringi.rdb", "start": 185980, "end": 250841}, {"filename": "/usr/lib/R/library/stringi/R/stringi.rdx", "start": 250841, "end": 254074}, {"filename": "/usr/lib/R/library/stringi/Meta/features.rds", "start": 254074, "end": 254206}, {"filename": "/usr/lib/R/library/stringi/Meta/package.rds", "start": 254206, "end": 255453}, {"filename": "/usr/lib/R/library/stringi/Meta/links.rds", "start": 255453, "end": 257870}, {"filename": "/usr/lib/R/library/stringi/Meta/nsInfo.rds", "start": 257870, "end": 259584}, {"filename": "/usr/lib/R/library/stringi/Meta/Rd.rds", "start": 259584, "end": 264299}, {"filename": "/usr/lib/R/library/stringi/Meta/hsearch.rds", "start": 264299, "end": 268816}, {"filename": "/usr/lib/R/library/stringi/libs/stringi.so", "start": 268816, "end": 3715146}, {"filename": "/usr/lib/R/library/stringi/libs/LICENSE.txt", "start": 3715146, "end": 3733379}, {"filename": "/usr/lib/R/library/stringi/help/AnIndex", "start": 3733379, "end": 3742856}, {"filename": "/usr/lib/R/library/stringi/help/aliases.rds", "start": 3742856, "end": 3744792}, {"filename": "/usr/lib/R/library/stringi/help/paths.rds", "start": 3744792, "end": 3745680}, {"filename": "/usr/lib/R/library/stringi/help/stringi.rdb", "start": 3745680, "end": 4193470}, {"filename": "/usr/lib/R/library/stringi/help/stringi.rdx", "start": 4193470, "end": 4195777}], "remote_package_size": 4195777, "package_uuid": "ca9ef91e-8855-45c4-90c8-b23e11a42bd1"});
   
   })();
   
+  var Module = typeof Module !== 'undefined' ? Module : {};
+  
+  if (!Module.expectedDataFileDownloads) {
+    Module.expectedDataFileDownloads = 0;
+  }
+  Module.expectedDataFileDownloads++;
+  (function() {
+    var loadPackage = function(metadata) {
+      var REMOTE_PACKAGE_NAME = "https://raw.githubusercontent.com/servo/rust-icu/master/icu-sys/icu/source/data/in/icudt55l.dat";
+      var REMOTE_PACKAGE_SIZE = 25908416;
+      function fetchRemotePackage(packageName, packageSize, callback, errback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', packageName, true);
+        xhr.responseType = 'arraybuffer';
+        xhr.onprogress = function(event) {
+          var url = packageName;
+          var size = packageSize;
+          if (event.total) size = event.total;
+          if (event.loaded) {
+            if (!xhr.addedTotal) {
+              xhr.addedTotal = true;
+              if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
+              Module.dataFileDownloads[url] = {
+                loaded: event.loaded,
+                total: size
+              };
+            } else {
+              Module.dataFileDownloads[url].loaded = event.loaded;
+            }
+            var total = 0;
+            var loaded = 0;
+            var num = 0;
+            for (var download in Module.dataFileDownloads) {
+            var data = Module.dataFileDownloads[download];
+              total += data.total;
+              loaded += data.loaded;
+              num++;
+            }
+            total = Math.ceil(total * Module.expectedDataFileDownloads/num);
+            if (Module['setStatus']) Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
+          } else if (!Module.dataFileDownloads) {
+            if (Module['setStatus']) Module['setStatus']('Downloading data...');
+          }
+        };
+        xhr.onerror = function(event) {
+          throw new Error("NetworkError for: " + packageName);
+        }
+        xhr.onload = function(event) {
+          if (xhr.status == 200 || xhr.status == 304 || xhr.status == 206 || (xhr.status == 0 && xhr.response)) { // file URLs can return 0
+            var packageData = xhr.response;
+            callback(packageData);
+          } else {
+            throw new Error(xhr.statusText + " : " + xhr.responseURL);
+          }
+        };
+        xhr.send(null);
+      };
+
+      function handleError(error) {
+        console.error('package error:', error);
+      };
+    
+        var fetchedCallback = null;
+        var fetched = null;
+        if (!fetched) fetchRemotePackage(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE, function(data) {
+          if (fetchedCallback) {
+            fetchedCallback(data);
+            fetchedCallback = null;
+          } else {
+            fetched = data;
+          }
+        }, handleError);
+      
+    function runWithFS() {
+  
+      function assert(check, msg) {
+        if (!check) throw msg + new Error().stack;
+      }
+  Module['FS_createPath']("/", "usr", true, true);
+Module['FS_createPath']("/usr", "lib", true, true);
+Module['FS_createPath']("/usr/lib", "R", true, true);
+Module['FS_createPath']("/usr/lib/R", "library", true, true);
+Module['FS_createPath']("/usr/lib/R/library", "stringi", true, true);
+Module['FS_createPath']("/usr/lib/R/library/stringi", "libs", true, true);
+
+          /** @constructor */
+          function DataRequest(start, end, audio) {
+            this.start = start;
+            this.end = end;
+            this.audio = audio;
+          }
+          DataRequest.prototype = {
+            requests: {},
+            open: function(mode, name) {
+              this.name = name;
+              this.requests[name] = this;
+              Module['addRunDependency']('fp ' + this.name);
+            },
+            send: function() {},
+            onload: function() {
+              var byteArray = this.byteArray.subarray(this.start, this.end);
+              this.finish(byteArray);
+            },
+            finish: function(byteArray) {
+              var that = this;
+      
+          Module['FS_createPreloadedFile'](this.name, null, byteArray, true, true, function() {
+            Module['removeRunDependency']('fp ' + that.name);
+          }, function() {
+            if (that.audio) {
+              Module['removeRunDependency']('fp ' + that.name); // workaround for chromium bug 124926 (still no audio with this, but at least we don't hang)
+            } else {
+              err('Preloading file ' + that.name + ' failed');
+            }
+          }, false, true); // canOwn this data in the filesystem, it is a slide into the heap that will never change
+  
+              this.requests[this.name] = null;
+            }
+          };
+      
+              var files = metadata['files'];
+              for (var i = 0; i < files.length; ++i) {
+                new DataRequest(files[i]['start'], files[i]['end'], files[i]['audio'] || 0).open('GET', files[i]['filename']);
+              }
+      
+        
+      function processPackageData(arrayBuffer) {
+        assert(arrayBuffer, 'Loading data file failed.');
+        assert(arrayBuffer instanceof ArrayBuffer, 'bad input to processPackageData');
+        var byteArray = new Uint8Array(arrayBuffer);
+        var curr;
+        
+          // Reuse the bytearray from the XHR as the source for file reads.
+          DataRequest.prototype.byteArray = byteArray;
+    
+            var files = metadata['files'];
+            for (var i = 0; i < files.length; ++i) {
+              DataRequest.prototype.requests[files[i].filename].onload();
+            }
+                Module['removeRunDependency']('datafile_stringi_icudt55l.dat');
+
+      };
+      Module['addRunDependency']('datafile_stringi_icudt55l.dat');
+    
+        if (fetched) {
+          processPackageData(fetched);
+          fetched = null;
+        } else {
+          fetchedCallback = processPackageData;
+        }
+      
+    }
+    if (Module['calledRun']) {
+      runWithFS();
+    } else {
+      if (!Module['preRun']) Module['preRun'] = [];
+      Module["preRun"].push(runWithFS); // FS is not initialized yet, wait for it
+    }
+  
+   }
+   loadPackage({"files": [{"filename": "/usr/lib/R/library/stringi/libs/icudt55l.dat", "start": 0, "end": 25908416}]});
+  })();
--- src/stringi/build.sh
+++ src/stringi/build.sh
@@ -11,8 +11,9 @@ cat << EOF > ~/.R/Makevars
 CC=emcc
 CXX=em++
 CXX11=em++
-CXX11FLAGS=-std=gnu++11 -D__STRICT_ANSI__ -I/app/build/R-4.1.2/include
-LDFLAGS=-s SIDE_MODULE=1 -s WASM_BIGINT -s ASSERTIONS=1
+CXX11FLAGS=-std=gnu++11 -Oz -D__STRICT_ANSI__ -I/app/build/R-4.1.2/include
+CFLAGS=-std=gnu11 -Oz -I/app/build/R-4.1.2/include
+LDFLAGS=-Oz -s SIDE_MODULE=1 -s WASM_BIGINT -s ASSERTIONS=1
 AR=emar
 EOF
 
@@ -30,5 +31,6 @@ rm "root/usr/lib/R/library/${PKG_NAME}/libs/icudt55l.dat"
 
 mkdir -p "../../dist/${PKG_NAME}/"
 file_packager "../../dist/${PKG_NAME}/${PKG_NAME}.data" --js-output="../../dist/${PKG_NAME}/${PKG_NAME}.js" --preload 'root@/' --use-preload-plugins
+cat stringi_data.js >> "../../dist/${PKG_NAME}/${PKG_NAME}.js"
 rm -rf root
 rm ~/.R/Makevars
